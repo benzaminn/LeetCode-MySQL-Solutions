05bba082a22acfec23a317345a0aeb944199a318
benzaminn <60427530+benzaminn@users.noreply.github.com>
2023-08-02 10:57:10 +0530

    Create LeetCode 1158. Market Analysis I.sql

---
+++ LeetCode 1158. Market Analysis I.sql
@@ -0,0 +1,49 @@
+-- Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+/*
+Example 1:
+
+Input: 
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2018-01-01 | Lenovo         |
+| 2       | 2018-02-09 | Samsung        |
+| 3       | 2018-01-19 | LG             |
+| 4       | 2018-05-21 | HP             |
++---------+------------+----------------+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2018-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2018-08-04 | 1       | 4        | 2         |
+| 5        | 2018-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+Items table:
++---------+------------+
+| item_id | item_brand |
++---------+------------+
+| 1       | Samsung    |
+| 2       | Lenovo     |
+| 3       | LG         |
+| 4       | HP         |
++---------+------------+ */
+
+
+
+select U.user_id as buyer_id,U.join_date,count(O.seller_id) as orders_in_2019
+from Users U
+left join (select * from Orders where 
+YEAR(order_date) =2019) O
+on U.user_id = O.buyer_id
+group by U.user_id
