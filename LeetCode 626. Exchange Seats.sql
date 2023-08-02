cda45da0736ccf399d1c405c3d0e15fb3ceb97de
benzaminn <60427530+benzaminn@users.noreply.github.com>
2023-08-02 10:12:15 +0530

    Create LeetCode 626. Exchange Seats.sql

---
+++ LeetCode 626. Exchange Seats.sql
@@ -0,0 +1,47 @@
+-- Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+-- Return the result table ordered by id in ascending order.
+
+-- The query result format is in the following example.
+
+ 
+/*
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+  */
+
+
+
+
+with cte as (
+select id,student,
+lead(student,1) over() as next,
+lag(student,1) over() as prev
+from seat
+)
+
+select id,
+case when id % 2 = 1 then coalesce(next,student)
+  else coalesce(prev,student) end as student
+from cte
+
