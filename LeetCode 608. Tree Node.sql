11757656d586e9c9fba473ea0152f7f2b65815cb
benzaminn <60427530+benzaminn@users.noreply.github.com>
2023-08-02 10:59:20 +0530

    Create LeetCode 608. Tree Node.sql

---
+++ LeetCode 608. Tree Node.sql
@@ -0,0 +1,40 @@
+-- Each node in the tree can be one of three types:
+
+-- "Leaf": if the node is a leaf node.
+-- "Root": if the node is the root of the tree.
+-- "Inner": If the node is neither a leaf node nor a root node.
+-- Write an SQL query to report the type of each node in the tree.
+
+-- Return the result table in any order.
+
+/*
+Input: 
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
+| 2  | 1    |
+| 3  | 1    |
+| 4  | 2    |
+| 5  | 2    |
++----+------+
+Output: 
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
++----+-------+ */
+
+
+select id,
+case when p_id is null then "Root"
+     when id in (select distinct p_id from Tree) and p_id is not null then "Inner"
+     else "Leaf"
+end as type
+from Tree
+order by id
