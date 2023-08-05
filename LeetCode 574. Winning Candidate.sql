12a6c6917ec9b9152c0d375a10fc9f03af2e2763
Suraj Sarkar <60427530+benzaminn@users.noreply.github.com>
2023-08-05 17:34:56 +0530

    Create LeetCode 574. Winning Candidate.sql

---
+++ LeetCode 574. Winning Candidate.sql
@@ -0,0 +1,47 @@
+
+
+
+-- Table: Candidate
+
+-- +-----+---------+
+-- | id  | Name    |
+-- +-----+---------+
+-- | 1   | A       |
+-- | 2   | B       |
+-- | 3   | C       |
+-- | 4   | D       |
+-- | 5   | E       |
+-- +-----+---------+  
+-- Table: Vote
+
+-- +-----+--------------+
+-- | id  | CandidateId  |
+-- +-----+--------------+
+-- | 1   |     2        |
+-- | 2   |     4        |
+-- | 3   |     3        |
+-- | 4   |     2        |
+-- | 5   |     5        |
+-- +-----+--------------+
+-- id is the auto-increment primary key,
+-- CandidateId is the id appeared in Candidate table.
+-- Write a sql to find the name of the winning candidate, the above example will return the winner B.
+
+-- +------+
+-- | Name |
+-- +------+
+-- | B    |
+-- +------+
+-- Notes:
+
+-- You may assume there is no tie, in other words there will be only one winning candidate
+
+select C.Name
+from candidate C 
+Left join vote V
+on C.id = V.CandidateId
+group by 1
+order by count(V.id) desc
+limit 1
+
+
